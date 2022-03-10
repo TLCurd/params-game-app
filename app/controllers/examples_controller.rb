021606cef7771831b48a374dbc4743ec0df32d92
TLCurd <TLCurdChiro@gmail.com>
2022-03-10 10:27:22 -0700

    adds guess a number game initial

--- app/controllers/examples_controller.rb
+++ app/controllers/examples_controller.rb
@@ -5,10 +5,27 @@ class ExamplesController < ApplicationController
     first = name.chars
     # p name.upcase 
     if first[0].downcase == "a"
-      render json: {message: "Your name starts with the first letter of the alphabet!"}
+      render json: {the_message: "Your name starts with the first letter of the alphabet!"}
     else
-      render json: {message: name.upcase}
+      render json: {the_message: name.upcase}
     end
   end
 
+  def guess_query
+    # render json: {the_message: "Guess a number"}
+    user_guess = 100
+    answer = 37
+
+    if params[:guess].to_i > answer
+      the_message = "Your guess is too high."
+    elsif params[:guess].to_i < answer
+      the_message = "Your gues is too low"
+    elsif params[:guess].to_i == answer
+      the_message = "You are correct!!!"
+    end
+
+    render json: {message: the_message}
+  end
+
 end
+ 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
   get "/query_params" => "examples#query"
-  
+  get "/guess_query" => "examples#guess_query"
 end
+ 
