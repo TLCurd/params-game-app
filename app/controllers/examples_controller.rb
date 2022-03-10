0f82d823c65817538aae0efc63597f6eb55be6fb
TLCurd <TLCurdChiro@gmail.com>
2022-03-09 19:21:57 -0700

    adds 'a' query route

--- app/controllers/examples_controller.rb
+++ app/controllers/examples_controller.rb
@@ -2,12 +2,13 @@ class ExamplesController < ApplicationController
   
   def query
     name = params[:name]
-    # first = name.split("")
-    p name.upcase 
-    render json: name.upcase 
-    # if first[0].downcase == "a"
-    #   p "Your name starts with the first letter of the alphabet!"
-    # end
+    first = name.chars
+    # p name.upcase 
+    if first[0].downcase == "a"
+      render json: {message: "Your name starts with the first letter of the alphabet!"}
+    else
+      render json: name.upcase
+    end
   end
 
 end
